0eb3d34e193d161ad6935fe2d88aa688e40363ed
반대준 <67678049+daejun4582@users.noreply.github.com>
2025-10-28 01:19:29 +0900

    feat : add redirecting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  redirect('/logoPage');
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace('/logoPage');
+  }, [router]);
+  
+  return null;
 }
